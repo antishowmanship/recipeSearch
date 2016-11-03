3b8f8c80e60e8bcd12cd7d04e881f24d88125f30
Ross Noren <ross.noren@gmail.com>
2016-11-03 17:09:29 -0400

    refined many-to-many relationships between ingredients and recipes and ingredients and grocery lists

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Recipe(models.Model):
-    owner = models.ForeignKey(User)
-    ingredients = models.ManyToManyField(Ingredient)
+
+class IngredientCategory(models.Model):
+    name = models.CharField(max_length=200, unique=True)
 
 
 class Ingredient(models.Model):
@@ -10,34 +11,38 @@ class Ingredient(models.Model):
     category = models.ForeignKey(IngredientCategory)
 
 
-class IngredientCategory(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+class Recipe(models.Model):
+    owner = models.ForeignKey(User)
+    ingredients = models.ManyToManyField(Ingredient, through=RecipeEntry)
+
+
+class RecipeEntry(models.Model):
+    ingredient = models.ForeignKey(Ingredient)
+    recipe = models.ForeignKey(Recipe)
+    quantity = models.FloatField()
+    unit = models.CharField(max_length=100)
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    recipes = models.ManyToManyField(Recipe)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
 
 class GroceryList(models.Model):
     name = models.CharField(max_length=200)
     expired = models.BooleanField(default=False)
     createdOn = models.DateTimeField(auto_now_add=True)
-    updatedOn = models.DateTimeField(auto_now_add=True, auto_now=True)
+    updatedOn = models.DateTimeField(auto_now=True)
+    entries = models.ManyToManyField(Ingredient, through=GroceryListEntry)
 
 
 class GroceryListEntry(models.Model):
     groceryList = models.ForeignKey(GroceryList, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     amount = models.FloatField()
-    ingredientUnit = models.ForeignKey(IngredientUnit)
+    ingredientUnit = models.CharField(max_length=50)
     checked = models.BooleanField(default=False)
     amount_selected = models.FloatField(default=0)
 
-
-class IngredientUnit(models.Model):
-    name = models.CharField(max_length=200)
-
-
-class IngredientConversion(models.Model):
-    start_unit = models.ForeignKey(IngredientUnit)
-    result_unit = models.ForeignKey(IngredientUnit)
-    multiplier = models.FloatField()
-
-
-class User(models.Model):
-    username = models.CharField(max_length=200, unique=True)
