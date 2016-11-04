b7c284e5cec01ecb56b06913418a4f4a9ee11e1f
Ross Noren <ross.noren@gmail.com>
2016-11-03 23:14:09 -0400

    added models to admin interface
    registered all existing models with the admin interface

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(IngredientCategory)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(RecipeEntry)
+admin.site.register(Menu)
+admin.site.register(GroceryList)
+admin.site.register(GroceryListEntry)
