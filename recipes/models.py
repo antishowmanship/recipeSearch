0552919aa5396cf1bed84634da18707a10eec60d
Ross Noren <ross.noren@gmail.com>
2016-11-03 22:58:54 -0400

    added __str__ methods
    added __str__ methods for all models

--- recipes/models.py
+++ recipes/models.py
@@ -5,15 +5,24 @@ from django.contrib.auth.models import User
 class IngredientCategory(models.Model):
     name = models.CharField(max_length=200, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=250, unique=True)
     category = models.ForeignKey(IngredientCategory)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     owner = models.ForeignKey(User)
     ingredients = models.ManyToManyField(Ingredient, through='RecipeEntry')
+
+    def __str__(self):
+        return "\n".join(self.ingredients)
 
 
 class RecipeEntry(models.Model):
@@ -22,12 +31,18 @@ class RecipeEntry(models.Model):
     quantity = models.FloatField()
     unit = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.ingredient + " " + str(self.quantity) + self.unit
+
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     recipes = models.ManyToManyField(Recipe)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class GroceryList(models.Model):
@@ -38,6 +53,9 @@ class GroceryList(models.Model):
     updatedOn = models.DateTimeField(auto_now=True)
     entries = models.ManyToManyField(Ingredient, through='GroceryListEntry')
 
+    def __str__(self):
+        return self.name
+
 
 class GroceryListEntry(models.Model):
     groceryList = models.ForeignKey(GroceryList, on_delete=models.CASCADE)
@@ -46,4 +64,7 @@ class GroceryListEntry(models.Model):
     ingredientUnit = models.CharField(max_length=50)
     checked = models.BooleanField(default=False)
     amount_selected = models.FloatField(default=0)
+
+    def __str(self):
+        return str(self.ingredient)
 
